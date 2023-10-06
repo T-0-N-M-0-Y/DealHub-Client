b7847876f5dd6d928cba78561e6680f0a04806e3
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-10-06 15:30:15 +0600

    Category Added

---
+++ src/Components/Category/Category.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Category = () => {
+    const [categories, setCategories] = useState([]);
+    console.log(categories);
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
+    return (
+        <div className="space-y-6">
+            <h2 className="text-2xl font-bold">BROWSE CATEGORIES</h2>
+            {
+                categories.map(category => <Link 
+                    className="block ml-4 text-xl" 
+                    key={category.id}
+                    to={`/category/${category.id}`}
+                    >{category.name}</Link>)
+            }
+        </div>
+    );
+};
+
+export default Category;
--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Category from "../Components/Category/Category";
 
 const MainLayout = () => {
   return (
     <div>
       <Outlet></Outlet>
+      <Category></Category>
     </div>
   );
 };
