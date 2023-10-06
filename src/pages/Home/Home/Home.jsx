39c92af990796aab2ea23f5845f08614a2669646
arafataft <arafatcd@gmail.com>
2023-10-06 21:42:32 +0600

    home page cover layout

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,32 @@
+import React from 'react';
+
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-4xl text-center font-bold">Hello DealHub</h1>
+    <>
+    
+      <div className="bg-gray-400 min-h-screen p-8">
+      <div className="flex">
+        {/* Categories */}
+        <div className="w-1/4 p-4">
+          <h2 className="text-2xl font-semibold mb-2">Categories</h2>
+
+        </div>
+        
+        {/* Cover Page */}
+        <div className="w-1/2 p-4">
+          <h2 className="text-2xl font-semibold mb-2">Cover Page</h2>
+          
+        </div>
+        
+        {/*Recent Products */}
+        <div className="w-1/4 p-4">
+          <h2 className="text-2xl font-semibold mb-2">Recent Products</h2>
+          
+        </div>
+      </div>
     </div>
+
+    </>
   );
 };
 
