939916144b3a22ad4a8b854386e284e5e49063f5
arafataft <arafatcd@gmail.com>
2023-10-07 01:49:33 +0600

    slider

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,44 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const Banner = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
+  const slides = [
+    {
+      imgSrc: 'https://tecdn.b-cdn.net/img/Photos/Slides/img%20(15).jpg',
+    },
+    {
+      imgSrc: 'https://tecdn.b-cdn.net/img/Photos/Slides/img%20(22).jpg',
+    },
+    {
+      imgSrc: 'https://tecdn.b-cdn.net/img/Photos/Slides/img%20(23).jpg',
+
+    },
+  ];
+
+  return (
+    <div className="banner">
+      <Slider {...settings}>
+        {slides.map((slide, index) => (
+          <div key={index}>
+            <img src={slide.imgSrc} className="block w-full" alt={`Slide ${index + 1}`} />
+            <div className="absolute inset-x-0 bottom-5 py-5 text-center text-white md:block">
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default Banner;
