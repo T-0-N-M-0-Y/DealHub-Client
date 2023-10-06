06b85eb82b1f1fcfe7aedaab3551c32ea6abd3d2
Sohag Rana <sohagrana921@gmail.com>
2023-10-06 22:58:21 +0600

    Layout Fixed

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
-import Category from "../Components/Category/Category";
 
 const MainLayout = () => {
   return (
     <div>
       <Outlet></Outlet>
-      <Category></Category>
     </div>
   );
 };
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
+import Category from "../../../Components/Category/Category";
 
 const Home = () => {
   return (
     <>
-    
-      <div className="bg-gray-400 min-h-screen p-8">
-      <div className="flex">
-        {/* Categories */}
-        <div className="w-1/4 p-4">
-          <h2 className="text-2xl font-semibold mb-2">Categories</h2>
+      <div className="min-h-screen p-2">
+        <div className="grid grid-cols-5 gap-2">
+          {/* Categories */}
+          <div className=" p-4 border-4">
+            <Category></Category>
+          </div>
+          <div className="col-span-4">
+            <div className="grid grid-cols-4 gap-2">
+              {/* Cover Page */}
+              <div className="col-span-3 p-4  border-4">
+                <h2 className="text-2xl font-semibold mb-2">Cover Page</h2>
+              </div>
 
-        </div>
-        
-        {/* Cover Page */}
-        <div className="w-1/2 p-4">
-          <h2 className="text-2xl font-semibold mb-2">Cover Page</h2>
-          
-        </div>
-        
-        {/*Recent Products */}
-        <div className="w-1/4 p-4">
-          <h2 className="text-2xl font-semibold mb-2">Recent Products</h2>
-          
+              {/*Recent Products */}
+              <div className=" p-4 border-4">
+                <h2 className="text-2xl font-semibold mb-2">Recent Products</h2>
+              </div>
+            </div>
+            {/* Category with icon */}
+            <div></div>
+          </div>
         </div>
       </div>
-    </div>
-
     </>
   );
 };
