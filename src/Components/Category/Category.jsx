ffd6e3b16067f54042b67aa3671be51db45b5824
Sohag Rana <sohagrana921@gmail.com>
2023-10-07 22:15:34 +0600

    Fixed

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,28 +1,30 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Category = () => {
-    const [categories, setCategories] = useState([]);
-    console.log(categories);
+  const [categories, setCategories] = useState([]);
+  console.log(categories);
 
-    useEffect(() => {
-        fetch('categories.json')
-            .then(res => res.json())
-            .then(data => setCategories(data))
-    }, [])
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
 
-    return (
-        <div className="space-y-6">
-            <h2 className="text-2xl font-bold">BROWSE CATEGORIES</h2>
-            {
-                categories.map(category => <Link 
-                    className="block text-xl font-semibold hover:bg-red-500 hover:text-white border p-1" 
-                    key={category.id}
-                    to={`/category/${category.id}`}
-                    >{category.name}</Link>)
-            }
-        </div>
-    );
+  return (
+    <div className="space-y-6">
+      <h2 className="text-2xl font-bold">BROWSE CATEGORIES</h2>
+      {categories.map((category) => (
+        <Link
+          className="block text-xl font-semibold hover:border-l-4 border-red-500 hover:text-red-500 p-1"
+          key={category.id}
+          to={`/category/${category.id}`}
+        >
+          {category.name}
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Category;
--- src/pages/Home/Navbar/Navbar.jsx
+++ src/pages/Home/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="bg-black text-white py-4 mb-4 px-6 text-xl  font-semibold">
+    <div className="bg-black opacity-90 text-white py-4 mb-4 px-6 text-xl  font-semibold">
       <div className="flex justify-evenly">
         <Link>HOME</Link>
         <Link>CONTACT</Link>
