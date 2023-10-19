b240cd63ac76a07e8177a0473f2b717218d72e9c
SalehinShohan <ps.shohan1@gmail.com>
2023-10-19 22:43:04 +0600

    add scroll top and popup

---
+++ src/Components/GoToTop/GoToTop.jsx
@@ -0,0 +1,11 @@
+import ScrollToTop from "react-scroll-to-top";
+
+const GoToTop = () => {
+  return (
+    <div>
+      <ScrollToTop smooth top="1500" color="blue" width="40" height="28" />
+    </div>
+  );
+};
+
+export default GoToTop;
--- src/pages/Home/Header/Header.jsx
+++ src/pages/Home/Header/Header.jsx
@@ -1,14 +1,19 @@
-import { useState, useEffect, useRef } from "react";
+import  { useState, useEffect, useRef } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BiSolidUser } from "react-icons/bi";
-import logo from "../../../assets/logo 2.png";
+import logo from "../../../assets/logo 2.png"; 
 
 function Header() {
   const [category, setCategory] = useState("All Category");
   const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
+  const [isMobileSearchOpen, setIsMobileSearchOpen] = useState(false);
   const userMenuRef = useRef(null);
 
-  const categoryOptions = ["All Category", "Name", "Music", "Audio"];
+  const categoryOptions = [
+    "All Category",
+    "Audio & Home Theater",
+    "Camera & Photo",
+  ];
 
   const handleCategoryChange = (value) => {
     setCategory(value);
@@ -16,6 +21,10 @@ function Header() {
 
   const toggleUserMenu = () => {
     setIsUserMenuOpen(!isUserMenuOpen);
+  };
+
+  const toggleMobileSearch = () => {
+    setIsMobileSearchOpen(!isMobileSearchOpen);
   };
 
   useEffect(() => {
@@ -36,13 +45,12 @@ function Header() {
     <div className="bg-white text-black py-4">
       <div className="container mx-auto flex justify-between items-center">
         <div>
-          {/* <h1 className="text-2xl font-bold">DealHub</h1> */}
           <img className="w-32" src={logo} alt="" />
         </div>
         <div className="flex items-center">
           <div className="relative inline-block text-white border">
             <select
-              className="bg-white text-black px-3 py-2 focus:outline-none"
+              className="bg-white text-black px-3 py-3 focus:outline-none"
               value={category}
               onChange={(e) => handleCategoryChange(e.target.value)}
             >
@@ -53,16 +61,38 @@ function Header() {
               ))}
             </select>
           </div>
-          <div className="divide-y-2 border">
+          {/* For medium and larger screens, show search input field */}
+          <div className=" border hidden md:inline">
             <input
               type="text"
-              className="bg-white text-black px-3 py-2 focus:outline-none"
+              className="bg-white text-black px-3 py-2 focus:outline-none md:mt-2"
               placeholder="I'm searching for..."
             />
+            <button
+              className="md:text-2xl md:mr-2"
+            >
+              
+              <AiOutlineSearch />
+            </button>
           </div>
-          <button className="px-3 py-2 relative lg:right-10 md:right-10">
-            <AiOutlineSearch />
-          </button>
+          {/* For mobile devices, show search icon and toggle search input on click */}
+          {isMobileSearchOpen ? (
+            <div className="divide-y-2 border md:hidden">
+              <input
+                type="text"
+                className="bg-white text-black px-3 py-3 focus:outline-none"
+                placeholder="I'm searching for..."
+              />
+              
+            </div>
+          ) : (
+            <button
+              className="px-3 py-2 md:hidden"
+              onClick={toggleMobileSearch}
+            >
+              <AiOutlineSearch />
+            </button>
+          )}
           <div
             className="ml-4 relative inline-block text-black"
             ref={userMenuRef}
